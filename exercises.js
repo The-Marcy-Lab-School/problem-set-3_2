ae51e7c9efaea595f5e386ba176c681c45248243
Ann Duong <thuyanannduong@gmail.com>
2020-11-02 15:35:54 -0500

    lint

--- exercises.js
+++ exercises.js
@@ -1,27 +1,27 @@
 function processReleaseData() {
-
+  //Write code here
 }
 
 function octalToDecimal() {
-
+  //Write code here
 }
 
 function anagram() {
-
+  //Write code here
 }
 
 function triangle() {
-
+  //Write code here
 }
 
 function fridayThe13ths() {
-
+  //Write code here
 }
 
 module.exports = {
-    processReleaseData,
-    octalToDecimal,
-    anagram,
-    triangle,
-    fridayThe13ths,
+  processReleaseData,
+  octalToDecimal,
+  anagram,
+  triangle,
+  fridayThe13ths,
 };
