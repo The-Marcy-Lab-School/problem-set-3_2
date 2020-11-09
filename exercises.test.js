1b3a393b77040a37bb768f7e5cd44dd7f530d943
Ann Duong <30392423+thuyanduong@users.noreply.github.com>
2020-11-09 07:47:49 -0500

    Update exercises.test.js

--- exercises.test.js
+++ exercises.test.js
@@ -35,7 +35,43 @@ test('Question 1: processReleaseData', () => {
         },
     ];
     expect(exercises.processReleaseData(newReleases)).toEqual([{ id: 70111470, title: 'Die Hard'}, { id: 675465, title: 'Fracture' }]);
+});
 
+// Question 1
+test('Question 1: processReleaseData again', () => {
+    const newReleases = [
+        {
+            'id': 654356453,
+            'boxart': 'http://cdn-0.nflximg.com/images/2891/BadBoys.jpg',
+            'uri': 'http://api.netflix.com/catalog/titles/movies/70111470',
+            'rating': [5.0],
+            'bookmark': [{ id:432534, time:65876586 }],
+        },
+        {
+            'title': 'The Chamber',
+            'boxart': 'http://cdn-0.nflximg.com/images/2891/TheChamber.jpg',
+            'uri': 'http://api.netflix.com/catalog/titles/movies/70111470',
+            'rating': [4.0],
+            'bookmark': [],
+        },
+        {
+            'id': 70111471,
+            'title': 'Die Hard',
+            'boxart': 'http://cdn-0.nflximg.com/images/2891/DieHard.jpg',
+            'uri': 'http://api.netflix.com/catalog/titles/movies/70111470',
+            'rating': [4.0],
+            'bookmark': [],
+        },
+        {
+            'id': 675466,
+            'title': 'Fracture',
+            'boxart': 'http://cdn-0.nflximg.com/images/2891/Fracture.jpg',
+            'uri': 'http://api.netflix.com/catalog/titles/movies/70111470',
+            'rating': [5.0],
+            'bookmark': [{ id:432534, time:65876586 }],
+        },
+    ];
+    expect(exercises.processReleaseData(newReleases)).toEqual([{ id: 70111471, title: 'Die Hard'}, { id: 675466, title: 'Fracture' }]);
 });
 
 // Question 2
